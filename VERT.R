2b424b1259302811d1f1559f23ad9083d22d8a92
awl75 <andrewlegan@gmail.com>
2025-03-27 14:43:24 -0700

    import res3, the summary results file from Mark with my edits

--- VERT.R
+++ VERT.R
@@ -1,4 +1,6 @@
 install.packages("readxl")
 library(readxl)
 
-vert_res <- read_excel("results2_asvs_normalized_MON_blastnedited.xlsx")
+vert_res2 <- read_excel("results2_asvs_normalized_MON_blastnedited.xlsx")
+
+vert_res3 <- read.delim("results3_asvs_merged_MON-vetted - vertebrates.tsv", sep = "\t", header = TRUE)
